57e3199665e111857c8f4d655479acb569370cb9
Joao Victor Fernandes <jovic258.fernandes@hotmail.com>
2021-09-09 01:06:37 -0300

    fixing map

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React, { useState, useEffect } from "react"
-import HttpClient from "./HttpClient"
 import './index.css';
+import axios from "axios";
 
 const App = () => {
-  const [images, setImages] = useState({})
-  
-  HttpClient.getImages().then(imagesData => {
-    setImages(imagesData.data)
-    console.log(imagesData.data)
+  const [images, setImages] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=YxrBRvxLaXoTRk8M0ehSLus5lQcXScSy2heOd1ci&start_date=2000-04-08&end_date=2000-06-08')
+    .then(res =>{
+      console.log(res)
+      res.data.reverse()
+      setImages(res.data)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
   })
-    
-  function handleSubmit(event) {
-    event.preventDefault(); 
-  }
 
   return(
     <div className="home">
       <h1 className="nasa-style">Spacestagram</h1>
       <div>
-      {images.map(() =>(
+      {images.map(item => (
           <div>
             <header className="title-date">
-              {apod.title}
+              {item.title}
             </header>
-            <img src={apod.url} className="image-day"/>
-            <p>{apod.date}</p>
-            <p>{apod.explanation}</p>
+            <img src={item.url} className="image-day"/>
+            <p>{item.date}</p>
+            <p>{item.explanation}</p>
           </div>
         ))}
       </div>
